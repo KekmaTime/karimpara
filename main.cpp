24663052edff92722680128be7a0890b90797986
Ananth B <22am014@sctce.ac.in>
2024-08-30 02:35:43 +0530

    feat(main): add test cases for vector3d class functionality in main.cpp
    - Included `#include vector3d.h` and `#include <iostream>` for necessary headers.
    - Added a `main()` function to test the functionality of the `vector3d` class

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include "vector3d.h"
+#include <iostream>
 
+int main() {
+  vector3d vec1;
+  vector3d vec2(1, 2, 3);
+  vector3d vec3(6, 5, 4);
+  std::cout << vec1 << vec2;
+  std::cout << vec2.length() << std::endl;
+  vec2.normalize();
+  std::cout << vec2;
+  vector3d vec4 = vec2 + vec3;
+  std::cout << vec4;
+  vec4 += vec2;
+  std::cout << vec4;
+}
